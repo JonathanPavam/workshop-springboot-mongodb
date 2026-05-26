03cadfd555fa555fdc9b497af0ea5ddfb6ea1216
Jonathan Pavam <pavamjonathan@gmail.com>
2026-05-25 22:01:34 -0300

    UserDTO to return users

--- src/main/java/com/pavam/workshopmongo/resources/UserResources.java
+++ src/main/java/com/pavam/workshopmongo/resources/UserResources.java
@@ -2,6 +2,7 @@ package com.pavam.workshopmongo.resources;
 
 
 import com.pavam.workshopmongo.domain.User;
+import com.pavam.workshopmongo.dto.UserDTO;
 import com.pavam.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users/")
@@ -17,8 +19,9 @@ public class UserResources {
     UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
