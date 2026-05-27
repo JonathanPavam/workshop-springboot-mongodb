c412549c85b6c60bef0ca57dcc3081e12ff16791
Jonathan Aragao Pavam <jonathan.pavam@fernandezpapel.com.br>
2026-05-27 16:33:39 -0300

    posts reference to user

--- src/main/java/com/pavam/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pavam/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -38,6 +39,7 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Hoje acordei feliz", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
 
-
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
