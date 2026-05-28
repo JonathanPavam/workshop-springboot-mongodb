088545af68c0018755a1a6fdef14675c183be676
JonathanPavam <pavamjonathan@gmail.com>
2026-05-28 12:28:57 -0300

    simple query with @Query

--- src/main/java/com/pavam/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pavam/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.pavam.workshopmongo.repository;
 
 import com.pavam.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,8 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post,String> {
 
 
+    @Query("{'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
     List<Post>findByTitleContainingIgnoreCase(String text);
 }
 
